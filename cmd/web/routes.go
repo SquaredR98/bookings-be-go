a6a55f6c5df12d135d8b746e6a748eb9b57cbd06
squaredr <ravi.ranjan@quicksell.co>
2023-10-24 09:46:31 +0530

    Replaced pat with chi router and Added few middlewares to the routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,11 +5,16 @@ import (
 
 	"github.com/SquaredR98/bookings-be/pkg/config"
 	"github.com/SquaredR98/bookings-be/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))

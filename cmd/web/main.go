fba4c4cd254a2c29de7f1ec06c1b87581c957291
squaredr <ravi.ranjan@quicksell.co>
2023-10-24 09:36:49 +0530

    Add pat router module to the project and used it for routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -29,9 +29,16 @@ func main() {
 
 	render.NewTemplate(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Printf("Server running on PORT: %s", PORT)
-	_ = http.ListenAndServe(PORT, nil)
+
+	srv := &http.Server{
+		Addr:    PORT,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
